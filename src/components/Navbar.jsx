26e835e54193d0d1f5f7a6fda5369fb36e01cec9
qudwns6122 <qudwns6122@naver.com>
2023-01-21 22:11:44 +0900

    firebase login,logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {FiShoppingBag} from 'react-icons/fi'
 import {BsFillPencilFill} from 'react-icons/bs'
+import { login, logout } from '../api/firebase.js'
+import { useState } from 'react';
 
 export default function Navbar() {
+    const [user, setUser] = useState();
+    const handleLogin = () => {
+        login().then(user => setUser(user));
+    }
+    const handleLogout = () => {
+        logout().then(user => setUser(user));
+    }
+
     return (
-        <header className='flex justify-between'>
-            <Link to='/'>
+        <header className='flex justify-between border-b border-gray-300 p-2'>
+            <Link to='/' className='flex items-center text-4xl text-brand'>
                 <FiShoppingBag/>
                 <h1>Shoppy</h1>
             </Link>
-            <nav>
+            <nav className='flex items-center gap-4 font-semibold'>
                 <Link to='/products'>Products</Link>
                 <Link to='/carts'>Carts</Link>
-                <Link to='/products/new'>
+                <Link to='/products/new' className='text-2xl'>
                     <BsFillPencilFill/>
                 </Link>
-                <button>Login</button>
+                {!user && <button onClick={handleLogin}>Login</button>}
+                {user && <button onClick={handleLogout}>Logout</button>}
             </nav>
         </header>
     );
