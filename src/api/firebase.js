6f80e4cb2bbb2d4caa2cbd4d53b6484b506f2ddf
qudwns6122 <qudwns6122@naver.com>
2023-01-22 18:46:52 +0900

    firebase database set

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,7 +6,8 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getDatabase, ref, set, child, get, onValue } from "firebase/database"
+import { getDatabase, ref, set, get, onValue } from "firebase/database";
+import {v4 as uuid } from 'uuid';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -54,6 +55,17 @@ async function adminUser(user) {
             }
             return user;
     });
+}
+
+export async function addNewProduct(product, imageUrl) {
+    const id = uuid();
+    set(ref(database, `products/${id}`), {
+        ...product,
+        id,
+        price: parseInt(product.price),
+        image: imageUrl,
+        options: product.options.split(','),
+    })
 }
 
 // 자동로그인 멈춤.
--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
 
@@ -14,6 +15,7 @@ export default function NewProducts() {
         e.preventDefault();
         uploadImage(file)
         .then(url => {
+            addNewProduct(product, url);
             console.log(url);
         })
     }
@@ -38,7 +40,7 @@ export default function NewProducts() {
             <input type='number' name='price' value={product.price ?? ''} placeholder='가격' required onChange={handleChange}/>
             <input type='text' name='category' value={product.category ?? ''} placeholder='카테고리' required onChange={handleChange}/>
             <input type='text' name='description' value={product.description ?? ''} placeholder='제품 설명' required onChange={handleChange}/>
-            <input type='text' name='option' value={product.option ?? ''} placeholder='옵션들(콤마(,)로 구분)' required onChange={handleChange}/>
+            <input type='text' name='options' value={product.options ?? ''} placeholder='옵션들(콤마(,)로 구분)' required onChange={handleChange}/>
             <Button text={'제품 등록하기'}/>
         </form>
     </section>
