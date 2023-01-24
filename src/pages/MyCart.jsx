9a46c7434abba47d08a9f17a8f656fe472be2cba
qudwns6122 <qudwns6122@naver.com>
2023-01-24 16:01:04 +0900

    carts tailwindCss

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -5,6 +5,7 @@ import { FaEquals } from 'react-icons/fa';
 import { getCart } from '../api/firebase';
 import CartItem from '../components/CartItem';
 import PriceCard from '../components/PriceCard';
+import Button from '../components/ui/Button';
 import { useAuthContext } from '../context/AuthContext';
 
 export default function MyCart() {
@@ -19,22 +20,23 @@ export default function MyCart() {
     const totalPrice = products && products.reduce((prev, current) => prev + parseInt(current.price) * current.quantity, 0);
     const SHIPPING = 3000;
     return (
-        <section>
-            <p>내 장바구니</p>
+        <section className='p-8 flex flex-col'>
+            <p className='text-2xl text-center font-bold pb-4 border-gray-300 border-b'>내 장바구니</p>
             {
                 !hasProducts && <p>장바구니에 상품이 없습니다.</p>
             }
             { hasProducts && <>
-                <ul>
+                <ul className='border-gray-300 border-b mb-8 px-8 p-4'>
                     {products && products.map((product) => <CartItem key={product.id} product={product} uid={uid}/>)}
                 </ul>
-                <div>
+                <div className='flex justify-between items-center px-2 md:px-8 mb-6 lg:px-16'>
                     <PriceCard text="상품 총액" price={totalPrice}/>
-                    <BsFillPlusCircleFill/>
+                    <BsFillPlusCircleFill className='shrink-0'/>
                     <PriceCard text="배송액" price={SHIPPING}/>
-                    <FaEquals/>
+                    <FaEquals className='shrink-0'/>
                     <PriceCard text="총가격" price={totalPrice + SHIPPING}/>
                 </div>
+                <Button text="주문하기"/>
             </>}
         </section>
     );
